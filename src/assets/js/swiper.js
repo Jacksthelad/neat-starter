0e93138a102f6f422d1456cf5740016f64b7f9b9
Jack Uterhark <jackuterhark@gmail.com>
2024-04-12 12:07:03 +0100

    slider added

---
+++ src/assets/js/swiper.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var mySwiper = new Swiper('.mySwiper', {
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        // Responsive breakpoints
+        breakpoints: {
+            640: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 40,
+            },
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 50,
+            },
+        }
+    });
+});
